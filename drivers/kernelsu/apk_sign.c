7759db4960f6acaec04c969b94cc31b0f6d3b87b
KernelSU-Next Patcher <kernelsu-patcher@localhost>
2026-03-08 00:05:16 +0530

    debug: bypass cert size and hash check temporarily

--- drivers/kernelsu/apk_sign.c
+++ drivers/kernelsu/apk_sign.c
@@ -89,7 +89,7 @@ static bool check_block(struct file *fp, u32 *size4, loff_t *pos, u32 *offset,
 	ksu_kernel_read_compat(fp, size4, 0x4, pos); // certificate length
 	*offset += 0x4 * 2;
 
-	if (true) {
+	if (*size4 == expected_size) {
 		*offset += *size4;
 
 #define CERT_MAX_LENGTH 1024
@@ -111,7 +111,7 @@ static bool check_block(struct file *fp, u32 *size4, loff_t *pos, u32 *offset,
 		bin2hex(hash_str, digest, SHA256_DIGEST_SIZE);
 		pr_info("sha256: %s, expected: %s\n", hash_str,
 			expected_sha256);
-		if (true) {
+		if (strcmp(expected_sha256, hash_str) == 0) {
 			return true;
 		}
 	}
@@ -209,7 +209,6 @@ static __always_inline bool check_v2_signature(char *path,
 			}
 		}
 		if (i == 0xffff) {
-			pr_info("error: cannot find eocd\n");
 			goto clean;
 		}
 	}
