04085ffca7786e13d506fafa2a272b4ceed8f115
KernelSU-Next Patcher <kernelsu-patcher@localhost>
2026-03-08 12:52:39 +0530

    fix: guard manual hook calls with CONFIG_KSU_MANUAL_HOOK

--- kernel/reboot.c
+++ kernel/reboot.c
@@ -290,7 +290,7 @@ static DEFINE_MUTEX(reboot_mutex);
  *
  * reboot doesn't sync: do that yourself before calling this.
  */
-#ifdef CONFIG_KSU
+#ifdef CONFIG_KSU_MANUAL_HOOK
 extern int ksu_handle_sys_reboot(int magic1, int magic2, unsigned int cmd, void __user **arg);
 #endif
 SYSCALL_DEFINE4(reboot, int, magic1, int, magic2, unsigned int, cmd,
@@ -311,7 +311,7 @@ SYSCALL_DEFINE4(reboot, int, magic1, int, magic2, unsigned int, cmd,
 			magic2 != LINUX_REBOOT_MAGIC2B &&
 			magic2 != LINUX_REBOOT_MAGIC2C))
 		return -EINVAL;
-		#ifdef CONFIG_KSU
+		#ifdef CONFIG_KSU_MANUAL_HOOK
 			if (ksu_handle_sys_reboot(magic1, magic2, cmd, (void __user **)&arg))
 				return 0;
 		#endif
